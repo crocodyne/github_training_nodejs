034ff3b33a97d455694f7a55b6ae590b45e1bec4
sisyphe1980 <sisyphe1980@gmail.com>
2026-04-04 19:53:48 +0200

    Add express exercice

---
+++ node_js/express/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const morgan = require('morgan')
+
+const app = express();
+const port = 3000;
+
+const logger = (req, res, next) => {
+    console.log("req -> " , req);
+    next()
+}
+//create a logger
+//__?__
+
+//use logger
+//__?__
+//app.use(morgan('dev'))
+//app.use(logger)
+
+app.get('/', (req, res) => {
+    //how to start to measure time 
+    console.time('time_e');
+    res.send("Hello");
+    //how to display time until here
+    console.timeEnd('time_e');
+})
+
+app.listen(port, () => {
+    console.log("App is running");
+})
